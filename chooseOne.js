b7f718d88b1c04062f6cdf8cfd3c6bcbba5e681f
Craig <cav9871@saintjoe.edu>
2015-02-10 20:50:08 -0500

    I changed the index file and am about to change this file to relflect index

--- chooseOne.js
+++ chooseOne.js
@@ -1,6 +1,6 @@
 //var courses = [['Austgen','Grube','Heinzman','Herre','Hofer','Samano','Zekovic'],['Davis', 'Gainey', 'Klump', 'Postma']];
 
-var courses = { "click": ['1', '2', '3', '4', '5', '6']};
+var courses = { "click1": ['1', '2', '3', '4', '5', '6']};
 
 function chooseOne(course) {
   var thisClass = courses[course];
@@ -8,5 +8,5 @@ function chooseOne(course) {
   var n = thisClass.length;
   var which = Math.floor(Math.random() * n);
 
-  print('Chosen: ' + thisClass[which]);
+  print('click1: ' + thisClass[which]);
 
